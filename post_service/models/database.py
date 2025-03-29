41e8d54a608818631220c1786d693d3b0010965a
Баранник Никита Павлович <npbarannik@edu.hse.ru>
2025-03-29 13:09:23 +0300

    Fix declarative_base()

--- post_service/models/database.py
+++ post_service/models/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 import os
 
 POSTGRES_URL = os.getenv("POSTGRES_URL", "postgresql://post_user:post_password@post_service_db:5432/post_db")
